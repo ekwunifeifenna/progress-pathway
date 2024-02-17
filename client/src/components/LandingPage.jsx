4ebfc48d1b29fbbecfdf01d8bb5e0ff0d1a28cc3
ekwunifeifenna <ifennaekwunife73@gmail.com>
2024-02-17 06:47:39 -0800

    Modify the landing page And also test connection to github from a new device

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import '../style/landing-page.css';
 
-const LandingPage = ({ isLoggedIn }) => {
+const LandingPage = () => {
   return (
-    <div>
+    <div className='landing-page'>
+
+      <div className='landing-page-text'>
       <h1>Progress Pathway</h1>
-      {isLoggedIn ? (
-        <Link to="/logout">Logout</Link>
-      ) : (
-        <>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
-        </>
-      )}
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore similique voluptatem, 
+        vitae alias culpa consequatur quisquam, aut molestiae est quod non reiciendis accusantium 
+        impedit distinctio voluptas aliquam facere magni at repellendus, quam aspernatur placeat suscipit illo.
+         Sunt, deleniti. Ratione sit in esse facere maiores iure quidem est totam dicta dolore.</p>
+
+         <button>Get Started</button>
+
+      </div>
+      
+      <img src={logo} alt='progress pathway logo'/>
+        
+      
     </div>
   );
 };
